2a1244fc0401c4329b35f1805758b9d9088a48da
henrique <henriquecordeiro054@gmail.com>
2023-07-26 18:49:50 -0300

    upload context usuario

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // App.js
 import React from 'react';
-import {  Routes, BrowserRouter, Route } from 'react-router-dom';
+import { Routes, BrowserRouter, Route } from 'react-router-dom';
 
 import Header from './componentes/Header';
 import Footer from './componentes/Footer';
@@ -13,33 +13,36 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 
 import { CarrinhoProvider } from './contextos/carrinho.js';
+import { UsuarioProvider } from './contextos/usario.js';
 
 
 function App() {
   return (
-   
-      <div className="App">
-        <BrowserRouter>
-         
+
+    <div className="App">
+      <BrowserRouter>
+        <UsuarioProvider>
           <CarrinhoProvider>
             <Header />
-              <Routes> 
-                      <Route exact path="/" element={<Home/>} />
-                      <Route path="/product/:id" element={<ProductDetails/>} />
-                      <Route path="/cart" element={<Cart/>} />
-                      
-                      <Route path="/sobre" element={<About titulo='Sobre Nos' />} />
-                      
-                      <Route path="/register" element={<Register />} />
-                      <Route path="/login" element={<Login />} />
-                      <Route path="*" element={<NotFound />}/>
-                </Routes>
-             <Footer />
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route path="/product/:id" element={<ProductDetails />} />
+              <Route path="/cart" element={<Cart />} />
+
+              <Route path="/sobre" element={<About titulo='Sobre Nos' />} />
+
+              <Route path="/register" element={<Register />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+            <Footer />
           </CarrinhoProvider>
-          </BrowserRouter>
-      
-      </div>
-    
+        </UsuarioProvider>
+      </BrowserRouter>
+
+
+    </div>
+
   );
 }
 
