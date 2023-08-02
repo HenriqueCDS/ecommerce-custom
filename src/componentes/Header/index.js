05bd781453099ddc68edc4f6279808a731a16e85
henrique <henriquecordeiro054@gmail.com>
2023-08-01 22:14:36 -0300

    update barraPesquisa

--- src/componentes/Header/index.js
+++ src/componentes/Header/index.js
@@ -3,17 +3,39 @@ import React from 'react';
 import header from './header.module.css';
 import { Heart, ShoppingCart, User } from 'phosphor-react';
 import { useCarrinhoContext } from '../../contextos/carrinho';
-import CabecalhoLink from './CabecalhoLink'
+import CabecalhoLink from './CabecalhoLink';
+import { useState } from 'react'
+import json from '../../json/produtos.json';
 
 function Header() {
+  const [livrosProdutos, setLivrosProdutos] = useState([])
+      
 
-  const { quantidadeProdutos } = useCarrinhoContext();
-  console.log('carrinho:', quantidadeProdutos);
   return (
     <header className={header.cabecalho}>
       <CabecalhoLink to="/">
-        Logo
+        <div className={header.logo}>
+          <h1>Shop<span>Now</span></h1>
+        </div>
       </CabecalhoLink>
+      <div className={header.pesquisa}>
+      <input
+        placeholder="O que você esta buscando"
+        onBlur={evento => {
+          const textoDigitado = evento.target.value;
+       
+          const resultadoPesquisa = json.length ? json.filter(json => json.name.includes(textoDigitado)) : []
+          setLivrosProdutos(resultadoPesquisa)
+        }}
+      />
+      <div className={header.result}>
+      {
+      livrosProdutos.map(produto => (
+          <li>{produto.name}</li>
+      ))}
+      </div>
+
+      </div>  
       <nav className={header.navbar}>
         <CabecalhoLink url='/'> <User size={28} />
           Home
@@ -22,13 +44,13 @@ function Header() {
         <CabecalhoLink url='/'> <Heart size={28} />
           Favoritos
         </CabecalhoLink>
-        
+
         <CabecalhoLink url='/cart'> <ShoppingCart size={28} />
-        Meus produtos 
+          Meus produtos
         </CabecalhoLink>
       </nav>
-        
-     
+
+
     </header>
 
   );
