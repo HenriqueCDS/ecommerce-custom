47a3685011e236c865bbb4d572b6adf8fc9433cd
gabriel <gabrieldecarvalhu@gmail.com>
2023-08-07 21:34:28 -0300

    add: rotacao de slides automatica

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
-import 'swiper/css/effect-fade'; 
+import 'swiper/css/effect-coverflow'; 
 register();
 
 
