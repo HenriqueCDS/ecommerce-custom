ea06b1385dbefbe1bb7db37353308a3257dff731
gabriel <gabrieldecarvalhu@gmail.com>
2023-08-21 21:00:39 -0300

    Adicina fontes e biblioteca framer-motion

--- src/componentes/CardList/index.js
+++ src/componentes/CardList/index.js
@@ -2,21 +2,28 @@
 import React from 'react';
 import Card from '../Card';
 import styles from './ProdutosLists.module.css';
-import products from '../../json/produtos.json'
+import products from '../../json/produtos.json';
+import { motion } from "framer-motion";
 function CardList() {
   
 
   return (
     <div className={styles.container}>
       {products.map((product) => (
-        
-        <Card
+        <motion.card
+        whileHover={{scale:1.1}}
+        whileTap={{scale:0.9}}        > 
+         <Card
           id={product.id}
           name={product.name}
           image={product.image}
           price={product.price}
         />
         
+
+        </motion.card>
+        
+       
       ))}
     </div>
   );
