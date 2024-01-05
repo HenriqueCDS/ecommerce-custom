79b66e38bd826cba076a68d76bb7005abda23e42
gabriel <gabrieldecarvalhu@gmail.com>
2024-01-05 15:38:41 -0300

    criando e arrumando services

--- src/Services/services.js
+++ src/Services/services.js
@@ -1,26 +1,65 @@
 import axios from 'axios';
+import { redirect } from 'react-router-dom';
 
-async function mandaDadosCadastro(){
-    try {
-        await axios.post('http://localhost:4000/users', {
-            email: "Gabriel@gmail.com",
-            fistName: "Gabriel",
-            lastName: "Barbosa",
-            password: "123",
-        })
-        .then(() => {
-            alert("Usuario cadastrado com sucesso!")
-        })
+
+async function mandaDadosCadastro({nome, sobreNome, email, senha}){
+        try {
+            await axios.post('http://localhost:4000/users', {
+                email: email,
+                fistName: nome,
+                lastName: sobreNome,
+                password: senha,
+            })
+            .then(() => {
+                alert('Usuario cadastrado');
+                window.location.href = '/';
+            })
+           
+        } catch (error) {
+            console.log('erro',error)
+        }
+       
         
-    } catch (error) {
-        console.log('erro',error)
-        
-    }
+    
   
 }
 
+
+
+async function pegaTodosCadastros(){
+        try {
+            let url = "http://localhost:4000/users";
+            const response = await axios.get(url)
+            .then(resposta => console.log(JSON.parse(resposta.request.response)))
+            .then(()=> {
+                alert('caiu then')
+                
+            })
+    
+        } catch (error) {
+            console.log('erro',error);
+        }
+    
+}
+
+// async function pegaTodosCadastros(){
+//     try {
+//         let url = "http://localhost:4000/users";
+//         await fetch(url)
+//             .then((res) => res.json())
+//             .then((data)=> {
+//                 console.log(data)
+//             })
+        
+//     } catch (error) {
+//         console.log('erro',error);
+//     }
+
+// }
+
 export {
-    mandaDadosCadastro
+    mandaDadosCadastro,
+    pegaTodosCadastros
 }
 
 
--- src/componentes/Cadastro/index.js
+++ src/componentes/Cadastro/index.js
@@ -1,9 +1,19 @@
 import style from './cadastro.module.css'
 import { User, EnvelopeSimple, LockSimple } from 'phosphor-react';
-import { mandaDadosCadastro } from '../../Services/services.js'
+import { mandaDadosCadastro, pegaTodosCadastros } from '../../Services/services.js'
+import { useState } from 'react';
 
 
 export default function Cadastro() {
+
+  const [nome, setNome] = useState("")
+  const [sobreNome, setSobreNome] = useState("")
+  const [email, setEmail] = useState("")
+  const [senha, setSenha] = useState("")
+
+  function resetaForm(){
+
+  }
   return (
     <div className={style.registro}>
     <form className={style.formlogin}>
@@ -11,13 +21,13 @@ export default function Cadastro() {
         <h3>Preencha seus dados</h3>
         
         <User size={28}  />
-        <input type="text" placeholder="Nome" name="uname" required /> 
-        <input type="text" placeholder="Sobre nome" name="uname" required /> 
+        <input type="text" placeholder="Nome" name="uname" onChange={(event) => setNome(event.target.value)} required /> 
+        <input type="text" placeholder="Sobre nome" name="uname" onChange={(event) => setSobreNome(event.target.value)} required /> 
         <EnvelopeSimple size={28} />
-        <input type="text" placeholder="Email" name="email" required />
+        <input type="text" placeholder="Email" name="email" onChange={(event) => setEmail(event.target.value)} required />
         <LockSimple size={28} />
-        <input type="password" placeholder="Senha" name="psw" required />
-        <button type="submit" onClick={() => mandaDadosCadastro()} >Cadastrar</button>
+        <input type="password" placeholder="Senha" name="psw" onChange={(event) => setSenha(event.target.value)} required />
+        <button type="submit" onClick={() => mandaDadosCadastro({nome, sobreNome, email, senha})} >Cadastrar</button>
     </form>
 
 </div>
